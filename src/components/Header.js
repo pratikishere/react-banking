3c99b18af0ce2a9cf78be9142b2fbe6b9771d55e
amit bidkar <amit.bidkar1@gmail.com>
2023-06-22 15:28:00 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <a href='#default' className='logo'>
+        HSBC Premier League
+      </a>
+    </div>
+  );
+}
---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUser((prevUser) => ({ ...prevUser, username, password }));
+    setUsername('');
+    setPassword('');
+    console.log(user);
+  };
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <form style={{ marginTop: '5%' }} onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username</label>
+        <input
+          id='username'
+          type='text'
+          onChange={(event) => setUsername(event.target.value)}
+          value={username}
+        />
+        <label htmlFor='username'>Password</label>
+        <input
+          id='password'
+          type='text'
+          value={password}
+          onChange={(event) => setPassword(() => event.target.value)}
+        />
+        <button
+          style={{
+            display: 'block',
+            position: 'absolute',
+            left: '50%',
+            marginTop: '20px',
+          }}
+          type='submit'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
